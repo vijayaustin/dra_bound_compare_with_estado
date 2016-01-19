c28040d6f3a1a0e6b916493d900a150a5dec8219
Vijay Jegaselvan <vjegase@us.ibm.com>
2016-01-18 20:57:18 -0600

    Changed stmts to grunt-idra2 + cleaned up

--- execute.sh
+++ execute.sh
@@ -38,36 +38,16 @@ function dra_logger {
     npm install grunt-cli
     #npm install grunt-idra
 	npm install grunt-idra2
-
-    #echo "Service List: ${DRA_SERVICE_LIST}"
+	
     echo -e ""
-
-    #dra_commands "${DRA_EVENT_TYPE_1}" "${DRA_FILE_1}" "${DRA_SERVER}"
-    #dra_commands "${DRA_EVENT_TYPE_2}" "${DRA_FILE_2}" "${DRA_SERVER}"
-    #dra_commands "${DRA_EVENT_TYPE_3}" "${DRA_FILE_3}" "${DRA_SERVER}"
-    #dra_commands "${DRA_EVENT_TYPE_1}" "${DRA_FILE_1}"
-    #dra_commands "${DRA_EVENT_TYPE_2}" "${DRA_FILE_2}"
-    #dra_commands "${DRA_EVENT_TYPE_3}" "${DRA_FILE_3}"
 	dra_commands "${DRA_SERVICE_LIST}"
-
-
-    
-    #grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType=istanbulCoverage -file=tests/coverage/reports/coverage-summary.json
-    #grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType=testComplete -deployAnalyticsServer=https://da-test.oneibmcloud.com    
 }
 
 function dra_commands {
     dra_grunt_command=""
     
-    #if [ -n "$1" ] && [ "$1" != " " ]; then
-        #echo -e "Service List: $1 is defined and not empty"
 		echo -e "DRA_ENABLE_BOUND_SERVICE value: $DRA_ENABLE_BOUND_SERVICE"
 		echo -e "DRA_ENABLE_COMPARE_APPS value: $DRA_ENABLE_COMPARE_APPS"
-		#dra_grunt_command='grunt --gruntfile=node_modules/grunt-idra/idra.js -statusCheck="'
-		#dra_grunt_command+=$1
-		#dra_grunt_command+='"'
-		#echo -e "Final command sent to grunt-iDRA to check services:\n"
-		#echo -e $dra_grunt_command
 		
 		event_variable='{"CF_ORG":"'
 		event_variable+=${CF_ORG}
@@ -82,38 +62,30 @@ function dra_commands {
 		event_variable+='","CF_SPACE_ID":"'
 		event_variable+=${CF_SPACE_ID}
 		event_variable+='"}'
-		#echo -e "\nEvent Variable: $event_variable"
+		echo -e "\nEvent Variable: $event_variable"
 		
 		event_to_file='echo $event_variable > deployInfo.json'
 		eval $event_to_file
 		echo -e "\nEvent file created:"
 		cat deployInfo.json
 		
-		send_event='grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType=deployInfo -file=deployInfo.json'
+		send_event='grunt --gruntfile=node_modules/grunt-idra2/idra.js -eventType=deployInfo -file=deployInfo.json'
 		echo -e "\nSending event to iDRA ...\n"
 		eval $send_event
 		
 		if [ ${DRA_ENABLE_BOUND_SERVICE} == true ]; then
 		
 			echo -e "\nChecked bound service box!\n"
-
-			#delete_criteria='curl -H "projectKey: ${DRA_PROJECT_KEY}" -H "Content-Type: application/json" -X DELETE http://da.oneibmcloud.com/api/v1/criteria?name=DRADeploy_BOUND_COMPARE'
-			#echo -e "\nDeleting existing criteria ...\n"
-			#eval $delete_criteria
 			
 			criteria_variable='{ "name": "DRADeploy_BOUND_COMPARE", "revision": 2, "project": "key", "mode": "decision", "rules": [ { "name": "Check for bound services", "conditions": [ { "eval": "_areApplicationBoundServicesAvailable", "op": "=", "value": true } ] } ] }'
-			#echo -e "\nCriteria Variable: $criteria_variable"
+			echo -e "\nCriteria Variable: $criteria_variable"
 			
 			criteria_to_file='echo $criteria_variable > criteriafile.json'
 			eval $criteria_to_file
 			echo -e "\nCriteria created:\n"
 			cat criteriafile.json
 			
-			#post_criteria='curl -H "projectKey: ${DRA_PROJECT_KEY}" -H "Content-Type: application/json" -X POST -d @criteriafile.json http://da.oneibmcloud.com/api/v1/criteria'
-			#echo -e "\nPosting criteria to API...\n"
-			#eval $post_criteria
-			
-			get_decision='grunt --gruntfile=node_modules/grunt-idra/idra.js -decision=dynamic -criteriafile=criteriafile.json'
+			get_decision='grunt --gruntfile=node_modules/grunt-idra2/idra.js -decision=dynamic -criteriafile=criteriafile.json'
 			echo -e "\nRequesting decision from API...\n"
 			eval $get_decision
 			RESULT2=$?
@@ -123,46 +95,25 @@ function dra_commands {
         fi
 		
 		if [ ${DRA_ENABLE_COMPARE_APPS} == true ]; then
-			
 			echo -e "\nChecked compare deployments box!\n"
 			event_name="$(echo -e "${IDS_STAGE_NAME}" | tr -d '[[:space:]]')"
 			event_name+='_'
 			event_name+="$(echo -e "${IDS_JOB_NAME}" | tr -d '[[:space:]]')"
 			
-			send_manifest_event='grunt --gruntfile=node_modules/grunt-idra/idra.js -eventType='
+			send_manifest_event='grunt --gruntfile=node_modules/grunt-idra2/idra.js -eventType='
 			send_manifest_event+=$event_name
 			send_manifest_event+=' -file='
 			send_manifest_event+=${DRA_MANIFEST_FILE}
 			echo -e "\nEvent created: $send_manifest_event\n"
 			echo -e "\nSending event to iDRA ...\n"
 			eval $send_manifest_event
-			
-			#delete_criteria='curl -H "projectKey: ${DRA_PROJECT_KEY}" -H "Content-Type: application/json" -X DELETE http://da.oneibmcloud.com/api/v1/criteria?name=DRADeploy_BOUND_COMPARE'
-			#echo -e "\nDeleting existing criteria ...\n"
-			#eval $delete_criteria
-			
-			#criteria_variable='{ "name": "DRADeploy_BOUND_COMPARE", "revision": 2, "project": "key", "mode": "decision", "rules": [ { "name": "Check for bound services", "conditions": [ { "eval": "_areApplicationBoundServicesAvailable", "op": "=", "value": true } ] } ] }'
-			#echo -e "\nCriteria Variable: $criteria_variable"
-			
-			#criteria_to_file='echo $criteria_variable > criteriafile.json'
-			#eval $criteria_to_file
-			#echo -e "\nCriteria created:\n"
-			#cat criteriafile.json
-			
-			#post_criteria='curl -H "projectKey: ${DRA_PROJECT_KEY}" -H "Content-Type: application/json" -X POST -d @criteriafile.json http://da.oneibmcloud.com/api/v1/criteria'
-			#echo -e "\nPosting criteria to API...\n"
-			#eval $post_criteria
-			
-			#get_decision='grunt --gruntfile=node_modules/grunt-idra/idra.js -decision=dynamic -criteriafile=criteriafile.json'
-			#echo -e "\nRequesting decision from API...\n"
-			#eval $get_decision
 		else
 			echo -e "\nUnchecked compare deployments box!\n"
         fi
 		
 		if [ -n "$1" ] && [ "$1" != " " ]; then
-			#echo -e "Service List: $1 is defined and not empty"
-			dra_grunt_command='grunt --gruntfile=node_modules/grunt-idra/idra.js -statusCheck="'
+			echo -e "Service List: $1 is defined and not empty"
+			dra_grunt_command='grunt --gruntfile=node_modules/grunt-idra2/idra.js -statusCheck="'
 			dra_grunt_command+=$1
 			dra_grunt_command+='"'
 			echo -e "Final command sent to grunt-iDRA to check services:\n"
@@ -194,10 +145,7 @@ function dra_commands {
 		else
 			return 0
 		fi
-		
-    #else
-        #echo -e "\nService List is not defined or is empty .. proceeding with deployment ..\n"
-    #fi
+
 }
 
 dra_logger
