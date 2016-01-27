83b260be5ea0aca3376b7c477cd5c7f9217f1567
Vijay Jegaselvan <vjegase@us.ibm.com>
2016-01-27 11:29:31 -0600

    Remove print CF_TOKEN

--- execute.sh
+++ execute.sh
@@ -36,7 +36,6 @@ set +x
 function dra_logger {
 
 	export CF_TOKEN=$(sed -e 's/^.*"AccessToken":"\([^"]*\)".*$/\1/' ~/.cf/config.json)
-	echo ${CF_TOKEN}
 	chmod 777 ${EXT_DIR}/*.py
     ${EXT_DIR}/is_dra_there.py ${PIPELINE_TOOLCHAIN_ID} "${CF_TOKEN}" "${IDS_PROJECT_NAME}"
 	IS_DRA_RESULT=$?
